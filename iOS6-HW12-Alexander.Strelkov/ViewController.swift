40630ad27b53f9915f39d0f490220adf30bc0edb
Alex <wild6158@gmail.com>
2022-05-21 13:00:41 +0300

    Made animating circlebars for work and relax with different colors.
    Made bars for work and relax with different colors.

--- iOS6-HW12-Alexander.Strelkov/ViewController.swift
+++ iOS6-HW12-Alexander.Strelkov/ViewController.swift
@@ -8,30 +8,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    let workingTime = 1500
-    let restTime = 300
-    var secondsRemaining = 4
+
+    var secondsRemaining = 0
     var timer = Timer()
+    var isWorkTime = true
+    let workAndRelaxtimes = ["Work": 1500, "Relax": 300]
     
     //MARK: - Outlets
     
     @IBOutlet weak var animatingTimerLabel: UILabel!
     
+    @IBAction func workButton(_ sender: UIButton) {
+        secondsRemaining = workAndRelaxtimes["Work"] ?? 0
+        workButtonLabel.setTitle("press PLAY", for: .normal)
+    }
+    
+    @IBOutlet weak var workButtonLabel: UIButton!
+    @IBOutlet weak var relaxButtonLabel: UIButton!
+    
+    @IBAction func relaxButton(_ sender: UIButton) {
+        isWorkTime = false
+        secondsRemaining = workAndRelaxtimes["Relax"] ?? 0
+        relaxButtonLabel.setTitle("press PLAY", for: .normal)
+    }
+    
     @IBAction func playPauseButton(_ sender: UIButton) {
-        
+
         timer.invalidate()
+        if isWorkTime {
+            setUpWorkCircularProgressBarView()
+        } else {
+            setUpRelaxCircularProgressBarView()
+        }
         
-        secondsRemaining = 1500
-        
-        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        if isWorkTime {
+            workButtonLabel.setTitle("Working", for: .normal)
+        } else if !isWorkTime {
+            relaxButtonLabel.setTitle("Relaxing..", for: .normal)
+        }
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
         
     }
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        setUpCircularProgressBarView()
     }
     
     //MARK: Timer function
@@ -42,7 +62,7 @@ class ViewController: UIViewController {
         
         minutes = (secondsRemaining % 3600) / 60
         seconds = (secondsRemaining % 3600) % 60
-        if secondsRemaining > 0 {
+        if secondsRemaining >= 0 {
             animatingTimerLabel.text = String(format: "%02d:%02d", minutes, seconds)
             secondsRemaining -= 1
         } else {
@@ -55,19 +75,29 @@ class ViewController: UIViewController {
     //MARK: CircularBar
     
     var circularProgressBarView: CircularProgressBarView!
-    var circularViewDuration: TimeInterval = 2
     
-    func setUpCircularProgressBarView() {
+    func setUpWorkCircularProgressBarView() {
         // set view
         circularProgressBarView = CircularProgressBarView(frame: .zero)
         // align to the center of the screen
         circularProgressBarView.center = view.center
         // call the animation with circularViewDuration
-        circularProgressBarView.createCircularPath()
-        circularProgressBarView.progressAnimation(duration: circularViewDuration)
+        circularProgressBarView.createWorkCircularPath()
+        circularProgressBarView.progressAnimation(duration: Double(secondsRemaining))
         // add this view to the view controller
         view.addSubview(circularProgressBarView)
     }
     
+    func setUpRelaxCircularProgressBarView() {
+        // set view
+        circularProgressBarView = CircularProgressBarView(frame: .zero)
+        // align to the center of the screen
+        circularProgressBarView.center = view.center
+        // call the animation with circularViewDuration
+        circularProgressBarView.createRelaxCircularPath()
+        circularProgressBarView.progressAnimation(duration: Double(secondsRemaining))
+        // add this view to the view controller
+        view.addSubview(circularProgressBarView)
+    }
 }
 
--- iOS6-HW12-Alexander.Strelkov/circularProgressBarView.swift
+++ iOS6-HW12-Alexander.Strelkov/circularProgressBarView.swift
@@ -19,7 +19,7 @@ class CircularProgressBarView: UIView {
             super.init(coder: coder)
         }
 
-func createCircularPath() {
+func createWorkCircularPath() {
         // created circularPath for circleLayer and progressLayer
     let circularPath = UIBezierPath(arcCenter: CGPoint(x: frame.size.width / 2.0, y: frame.size.height / 2.0), radius: 80, startAngle: -Double.pi / 2, endAngle: 3 * Double.pi / 2, clockwise: true)
         // circleLayer path defined to circularPath
@@ -39,21 +39,46 @@ func createCircularPath() {
         progressLayer.lineCap = .round
         progressLayer.lineWidth = 10.0
         progressLayer.strokeEnd = 0
-        progressLayer.strokeColor = UIColor.green.cgColor
+        progressLayer.strokeColor = UIColor.orange.cgColor
         // added progressLayer to layer
         layer.addSublayer(progressLayer)
 }
     
+    func createRelaxCircularPath() {
+            // created circularPath for circleLayer and progressLayer
+        let circularPath = UIBezierPath(arcCenter: CGPoint(x: frame.size.width / 2.0, y: frame.size.height / 2.0), radius: 80, startAngle: -Double.pi / 2, endAngle: 3 * Double.pi / 2, clockwise: true)
+            // circleLayer path defined to circularPath
+            circleLayer.path = circularPath.cgPath
+            // ui edits
+            circleLayer.fillColor = UIColor.clear.cgColor
+            circleLayer.lineCap = .round
+            circleLayer.lineWidth = 20.0
+            circleLayer.strokeEnd = 1.0
+            circleLayer.strokeColor = UIColor.white.cgColor
+            // added circleLayer to layer
+            layer.addSublayer(circleLayer)
+            // progressLayer path defined to circularPath
+            progressLayer.path = circularPath.cgPath
+            // ui edits
+            progressLayer.fillColor = UIColor.clear.cgColor
+            progressLayer.lineCap = .round
+            progressLayer.lineWidth = 10.0
+            progressLayer.strokeEnd = 0
+            progressLayer.strokeColor = UIColor.green.cgColor
+            // added progressLayer to layer
+            layer.addSublayer(progressLayer)
+    }
+    
     func progressAnimation(duration: TimeInterval) {
-            // created circularProgressAnimation with keyPath
-            let circularProgressAnimation = CABasicAnimation(keyPath: "strokeEnd")
-            // set the end time
-            circularProgressAnimation.duration = duration
-            circularProgressAnimation.toValue = 1.0
-            circularProgressAnimation.fillMode = .forwards
-            circularProgressAnimation.isRemovedOnCompletion = false
-            progressLayer.add(circularProgressAnimation, forKey: "progressAnim")
-        }
+        // created circularProgressAnimation with keyPath
+        let circularProgressAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        // set the end time
+        circularProgressAnimation.duration = duration
+        circularProgressAnimation.toValue = 1.0
+        circularProgressAnimation.fillMode = .forwards
+        circularProgressAnimation.isRemovedOnCompletion = false
+        progressLayer.add(circularProgressAnimation, forKey: "progressAnim")
+    }
 }
 
 
